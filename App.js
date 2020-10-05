2e09be5d31de19f4a0659e328f4b7cd4c72a869a
Your Name <you@example.com>
2020-10-06 06:24:26 +0700

    Latihan

--- App.js
+++ App.js
@@ -1,107 +1,16 @@
-import React from 'react'
-import {View, Text, Image, Button, ScrollView, TouchableOpacity, StyleSheet} from 'react-native'
-
-class App extends React.Component {
+import React, { Component } from 'react'
+import { Text, StyleSheet, View } from 'react-native'
+import Login from "./src/Components/login"
+import Register from "./src/Components/register"
+ class App extends Component {
   render() {
     return (
-      <View>
-      <View style = {style.view1}>
-      <TouchableOpacity onPress = {() => alert('KOSONG')}>
-        <Image style = {{height: 15, width: 25, marginLeft: 2}}
-          source = {require('./src/assets/a.png')}
-        />
-        </TouchableOpacity>
-        <Text style = {style.text}>Kakak Asuh Indonesia</Text>
-      </View>
-      <ScrollView horizontal = {true} style = {{flexDirection: 'row'}}>
-      <TouchableOpacity onPress = {() => alert('Pilih Adik Asuh')} style = {style.view2}>
-       <Image style = {{height: 35, width: 35, marginTop: 13}}
-          source = {require('./src/assets/d.png')}
-        />
-        <Text>Pilih Adik Asuh</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('Santri Mandiri Ada 83')} style = {style.view2}>
-         <Text style = {{marginTop: 20, fontWeight: "bold", fontSize: 20}}>83</Text>
-         <Text>Santri Mandri</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('Santri Belum Mandiri Ada 71')} style = {style.view2}>
-         <Text style = {{marginTop: 20, fontWeight: "bold", fontSize: 20}}>71</Text>
-         <Text style = {{textAlign: 'center'}}>Santri Belum Mandri</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('Calon Santri Ada 45')} style = {style.view2}>
-         <Text style = {{marginTop: 20, fontWeight: "bold", fontSize: 20}}>45</Text>
-         <Text>Calon Santri</Text>
-      </TouchableOpacity>
-      </ScrollView>
-      <Text style = {{marginTop: 15, marginLeft: 10, fontWeight: 'bold', fontSize: 20}}>Adik Asuh Saya</Text>
-      <View style = {{backgroundColor: 'aqua', height: 90, flexDirection: 'row',margin: 7, alignItems: 'center', justifyContent: 'space-between', borderRadius: 10}}>
-      <TouchableOpacity onPress = {() => alert('Nama Adik Asuh : Jujun Setiawan')} style = {{marginLeft: 5}}>
-        <Image style = {{height: 50, width: 50, alignSelf: 'center'}}
-          source = {require('./src/assets/avatar.png')}
-        />
-        <Text>Nama Adik Asuh</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('Transfer Via BNI')} style = {{marginRight: 15}}>
-        <Image style = {{height: 50, width: 50, alignSelf: 'center'}}
-          source = {require('./src/assets/c.png')}
-        />
-        <Text>Transfer</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('Telah Terkonfirmasi')} style = {{marginRight: 5}}>
-        <Image style = {{height: 50, width: 50, borderRadius: 25, alignSelf: 'center'}}
-          source = {require('./src/assets/e.jpg')}
-        />
-        <Text>Konfirmasi</Text>
-      </TouchableOpacity>
-      </View>
-      <View style = {{backgroundColor: 'aqua', height: 90, flexDirection: 'row',margin: 7, alignItems: 'center', justifyContent: 'space-between', borderRadius: 10}}>
-      <TouchableOpacity onPress = {() => alert('KOSONG')} style = {{marginLeft: 5}}>
-        <Image style = {{height: 40, width: 40, alignSelf: 'center'}}
-          source = {require('./src/assets/a.png')}
-        />
-        <Text>Nama Adik Asuh</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('KOSONG')} style = {{marginRight: 15}}>
-        <Image style = {{height: 40, width: 40, alignSelf: 'center'}}
-          source = {require('./src/assets/a.png')}
-        />
-        <Text>Transfer</Text>
-      </TouchableOpacity>
-      <TouchableOpacity onPress = {() => alert('KOSONG')} style = {{marginRight: 5}}>
-        <Image style = {{height: 40, width: 40, borderRadius: 25, alignSelf: 'center'}}
-          source = {require('./src/assets/a.png')}
-        />
-        <Text>Konfirmasi</Text>
-      </TouchableOpacity>
-      </View>
-      </View>
+      <Login/>
+      // <Register/>
     )
-  } 
+  }
 }
 
-export default App
+const styles = StyleSheet.create({})
 
-const style = StyleSheet.create({
-  view1: { 
-    backgroundColor: 'dodgerblue',
-    flexDirection: 'row',
-    height: 35,
-    alignItems: "center"
-  },
-  text: {
-    color: 'white',
-    fontWeight: 'bold',
-    fontSize: 19,
-    marginLeft: 6,
-    marginBottom: 2,
-  },
-  view2: {
-    backgroundColor: 'aqua',
-    height: 80,
-    width: 90,
-    alignItems: 'center',
-    marginTop: 5,
-    marginLeft: 5,
-    borderRadius: 10,
-  },
-})
+export default App;
---
+++ src/Components/login.js
@@ -0,0 +1,42 @@
+import React from "react"
+import {View,Text,Button,TouchableOpacity,TextInput,StyleSheet,Image} from "react-native"
+import styles from "./style.js"
+class Login extends React.Component {
+  render() {
+    return (
+      <View>
+        <View style = {styles.view1}>
+          <Text style = {styles.text1}>My App</Text>
+        </View>
+        <View style = {styles.view2}>
+        <TouchableOpacity onPress = {() => alert("ini gambar")}>
+        <Image style = {styles.image1}
+          source = {require("../assets/native.jpg")}/>
+        </TouchableOpacity>
+        <Text style = {styles.text2}>Learn React Native</Text>
+        <Text style = {{color: "white", marginTop: 65, marginLeft: 65}}>Email :</Text>
+        <View style = {styles.view3}>
+          <TextInput placeholder = "Masukan Alamat Email Anda" style = {{marginLeft: 15, marginTop: 5, borderColor: "white"}}/>
+        </View>
+        <Text style = {{color: "white", marginTop: 15, marginLeft: 65}}>Password :</Text>
+        <View style = {styles.view3}>
+          <TextInput secureTextEntry = {true} placeholder = "Masukan Password" style = {{marginTop: 5, marginLeft: 15, borderColor: "white"}}/>
+        </View>
+        <TouchableOpacity onPress = {() => alert("masuk ke halaman lupa password")}>
+        <Text style = {{color: "white", alignSelf: "center", marginLeft: 140, marginTop: 1}}>Lupa Password ?</Text>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {() => alert("Login Sukses")}>
+          <View style = {styles.view4}>
+            <Text style = {{color: "white", alignSelf: "center", fontWeight: "bold", fontSize: 20, marginTop: 5}}>Login</Text>
+          </View>
+        </TouchableOpacity>
+        <TouchableOpacity onPress = {() => alert("Membuat Akun")}>
+          <Text style = {{color: "white", alignSelf: "center", marginTop: 5, }}>Tidak Punya Akun ? Buat Akun</Text>
+        </TouchableOpacity>
+        </View>
+      </View>
+    )
+  }
+}
+
+export default Login;
